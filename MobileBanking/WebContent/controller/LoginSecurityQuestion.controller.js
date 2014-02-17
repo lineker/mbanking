2b23e68c9f8f75ae2f82d2ece3b8593450f98b44
Tomazeli, Lineker <lineker.tomazeli@sap.com>
2014-02-17 12:33:42 -0500

    handling response for second login page.

--- MobileBanking/WebContent/controller/LoginSecurityQuestion.controller.js
+++ MobileBanking/WebContent/controller/LoginSecurityQuestion.controller.js
@@ -1,5 +1,5 @@
-/*jQuery.sap.require("model.SapBeans");
-jQuery.sap.require("util.sapconnectors.MBSecurityConnector");*/
+jQuery.sap.require("model.SapBeans");
+jQuery.sap.require("util.sapconnectors.MBSecurityConnector");
 sap.ui.controller("controller.LoginSecurityQuestion", {
 
 /**
@@ -36,7 +36,61 @@ sap.ui.controller("controller.LoginSecurityQuestion", {
 //
 //	}
 	
+	processAnswerMultifactorSecurityInfoResponse : function(response, textStatus, jqXHR) {
+		console.log("success");
+		console.log(response);
+		
+		var xml = jqXHR.responseText;
+		var xmlDoc = $.parseXML(xml);
+		var $xml = $(xmlDoc);
+		var mbUser = new model.SapBeans.MBUser($xml);
+		var $extraMap = $xml.find("ns1\\:extraMap,extraMap");
+		var $entries = $extraMap.find("ns1\\:entry,entry");
+		$entries.each(function() {
+			var value = $(this).text();
+			var mapKey = '';
+			$.each(this.attributes, function() {
+				if ($(this).val() != "xsd:string") {
+					mapKey = $(this).val();
+					if (mapKey === 'secretPhrase') {
+						mbUser.passphrase = value;
+					} else if (mapKey === 'passmarkImgSrc') {
+						mbUser.imageUrl = value;
+					}
+				}
+			});
+		});
+		var $challenge = $xml.find("ns0\\:challenge,challenge");
+		var question2 = $challenge.text();
+		
+		mbUser.pmdata = mbUser.extra.extra['enc_pmdata'];
+		var userModel = new sap.ui.model.json.JSONModel(mbUser);  
+        sap.ui.getCore().setModel(userModel, "USER");
+        console.log(mbUser);
+		if (question2) {
+			currentSession.question = question2;
+			//$.mobile.changePage("#loginmfa");
+			//not sure where this goes.
+		} else {
+			//go to last page
+			var bus = sap.ui.getCore().getEventBus();
+	        bus.publish("nav", "to", { 
+	            id : "LoginPwd"
+	        });
+		}
+	},
+	
+	processHandleError : function(response) {
+		console.log("error");
+		console.log(response);
+	},
+	
 	continueLogin :  function(oEvent) { 
-        
+        var user = sap.ui.getCore().getModel("USER").getData();
+        console.log(user);
+        user.multifactorInfo[0].response = sap.ui.getCore().byId("securityQuestionAnswer").getValue();
+        user.multifactorInfo[0].enumClass = 'ChallengeQuestion';
+        user.multifactorInfo[0].extra.extra['regDevice'] = 'false';
+        util.sapconnectors.MBSecurityConnector.answerMultifactorSecurityInfo(user, this.processAnswerMultifactorSecurityInfoResponse, this.processHandleError);
     }
 });
